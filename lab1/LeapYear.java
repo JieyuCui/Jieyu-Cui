8cb48c1c463aa8c808264d012557ebed452356f4
Jieyu <jieyu_cui@berkeley.edu>
2020-06-24 15:39:38 -0700

    done

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author JIEYU CUI
  */
 public class LeapYear {
 
@@ -11,6 +11,16 @@ public class LeapYear {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 != 0 && year % 4 == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 
